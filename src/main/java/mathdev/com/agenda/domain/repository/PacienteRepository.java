63dfc61fcc907c8884b8fad8818fb974179489c9
matheusor99 <matheusor99@gmail.com>
2023-10-06 10:59:34 -0300

    repository e service de Paciente

---
+++ src/main/java/mathdev/com/agenda/domain/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package mathdev.com.agenda.domain.repository;
+
+import mathdev.com.agenda.domain.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
---
+++ src/main/java/mathdev/com/agenda/domain/service/PacienteService.java
@@ -0,0 +1,37 @@
+package mathdev.com.agenda.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import mathdev.com.agenda.domain.entity.Paciente;
+import mathdev.com.agenda.domain.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PacienteService {
+
+    @Autowired
+    private final PacienteRepository repository;
+
+
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
